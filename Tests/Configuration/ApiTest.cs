b05d658f96ba84936fc38594f19078afb80e087a
JValck <jasper_de_valck@hotmail.com>
2020-11-03 18:59:52 +0100

    Start test suite

---
+++ Tests/Configuration/ApiTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Tests.Configuration
+{
+    public class ApiTest
+    {
+        [Fact]
+        public void ApiReturnsNewConfigurationIfNotCalledYet()
+        {
+            var result = Mailocomotive.Configuration.Api.Configuration();
+            Assert.NotNull(result);            
+        }
+
+        [Fact]
+        public void ApiReturnsExistingConfigurationIfCalledInThePast()
+        {
+            var first = Mailocomotive.Configuration.Api.Configuration();
+            var second = Mailocomotive.Configuration.Api.Configuration();            
+            Assert.True(first.Equals(second));
+        }
+    }
+}
