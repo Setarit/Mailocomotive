25e936e1c8791af7f33b06abdb0e70cd96f9e896
JValck <jasper_de_valck@hotmail.com>
2020-11-08 09:06:30 +0100

    SmtpMailProviderCollection test

---
+++ Tests/Setting/Multiple/Smtp/SmtpMailProviderCollectionTest.cs
@@ -0,0 +1,24 @@
+﻿using Mailocomotive.Setting.Multiple.Smtp;
+using Mailocomotive.Setting.Single;
+using System.Linq;
+using Xunit;
+
+namespace Tests.Setting.Multiple.Smtp
+{
+    public class SmtpMailProviderCollectionTest
+    {
+        [Fact]
+        public void GetCollectionReturnsArrayOfSmtpMailProviders()
+        {
+            var singleProvider = new SmtpMailProvider();
+            var provider = new SmtpMailProviderCollection();
+            provider.UseCollection(new MailProvider[] { singleProvider });
+
+            var returned = provider.GetCollection();
+            Assert.Single(returned);
+            var first = returned.First();
+            Assert.NotNull(first);
+            Assert.IsType(singleProvider.GetType(), first);
+        }
+    }
+}
